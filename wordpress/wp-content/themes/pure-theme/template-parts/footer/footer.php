5c36bfe009eac2ac8419261980233e74f63a362e
Vladimir <freanky.kanistra@gmail.com>
2026-06-17 01:34:01 +0300

    upd style and header, footer

--- wordpress/wp-content/themes/pure-theme/template-parts/footer/footer.php
+++ wordpress/wp-content/themes/pure-theme/template-parts/footer/footer.php
@@ -94,6 +94,56 @@ $footerStyle = get_field('footer_style', $id);
                     </div>
                 </footer>
             <?php else: ?>
+                <footer class="footer footer-brand footer-center<?= $customClass; ?>">
+                    <?php if ( $showBg ):
+                        $suddenBgDesk = get_field('footer_bg_img_desk', $id);
+                        $suddenBgMob = get_field('footer_bg_img_mob', $id);
+
+                        $bgArgs = [
+                            'img' => $suddenBgDesk,
+                            'img_mob' => $suddenBgMob,
+                            'section' => 'footer-brand',
+                        ];
+
+                        get_template_part( 'components/image', null, $bgArgs );
+                    endif; ?>
+
+                    <div class="container footer-container grid-container">
+                        <div class="footer-wrapper footer-center-wrapper">
+                            <div class="footer-content-wrapper footer-center-content-wrapper">
+                                <div class="logo footer-logo">
+                                    <?php get_template_part( 'components/logo' );
+
+                                    if ( !empty($logoText) ): ?>
+                                        <div class="after-logo">
+                                            <span><?= $logoText; ?></span>
+                                        </div>
+                                    <?php endif; ?>
+                                </div>
+
+                                <?php if ( !empty($footerText) ): ?>
+                                    <div class="footer-content footer-center-content">
+                                        <?= $footerText; ?>
+                                    </div>
+                                <?php endif; ?>
+
+                                <?php if ( has_nav_menu( 'policy' ) ): ?>
+                                    <div class="privacy-navigation-wrapper footer-center-policy">
+                                        <?php get_template_part( 'components/navigation', null, ['menu' => 'policy'] ); ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+
+                            <div class="copyright-wrapper footer-center-copyright">
+                                <?php if ( !empty($copyright) ): ?>
+                                    <div class="copyright-item">
+                                        <span><?php echo $copyright; ?></span>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             <?php endif; ?>
         <!-- Wrapper End -->
         </div>
--- wordpress/wp-content/themes/pure-theme/template-parts/header/header.php
+++ wordpress/wp-content/themes/pure-theme/template-parts/header/header.php
@@ -141,4 +141,75 @@ endif;
 				</div>
 			</header>
 		<?php else: ?>
+			<header class="header header-brand header-center">
+				<div class="header-container-brand container grid-container">
+					<div class="header-center-group">
+						<div class="logo header-logo">
+							<?php get_template_part( 'components/logo' );
+
+							if ( !empty($logoText) ): ?>
+								<div class="after-logo">
+									<span><?= $logoText; ?></span>
+								</div>
+							<?php endif; ?>
+						</div>
+
+						<div class="header-menu-wrapper header-center-menu">
+							<div class="header-menu menu menu-body">
+								<div class="mobile-menu-wrapper">
+									<?php if ( !$customNav ):
+										if ( has_nav_menu( 'primary' ) ): ?>
+											<div class="nav-mobile-wrapper">
+												<?php get_template_part( 'components/navigation' ); ?>
+											</div>
+										<?php endif;
+									else: ?>
+										<div class="nav-mobile-wrapper">
+											<?php get_template_part( 'components/navigation', null, ['menu' => $landingNavMenu] ); ?>
+										</div>
+									<?php endif;
+
+									if ( !empty($enrollLink) ):
+										$target = $enrollLink['target'] ? $enrollLink['target']: '_self';
+										$url = $enrollLink['url'] ? $enrollLink['url']: '';
+										$descr = $enrollLink['title'] ? $enrollLink['title']: '';
+										$btnArgs = [
+											'label' => $descr,
+											'type' => 'primary',
+											'link' => $url,
+											'target' => $target,
+										];
+									?>
+
+									<div class="btn-wrapper">
+										<?php get_template_part( 'components/button', null, $btnArgs ); ?>
+									</div>
+								<?php endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="header-right-panel">
+						<?php if ( !empty($registerLink) ):
+							$target = $registerLink['target'] ? $registerLink['target']: '_self';
+							$url = $registerLink['url'] ? $registerLink['url']: '';
+							$descr = $registerLink['title'] ? $registerLink['title']: '';
+							$btnArgs = [
+								'label' => $descr,
+								'type' => 'secondary',
+								'link' => $url,
+								'target' => $target,
+							];
+						?>
+						<div class="header-link">
+							<?php get_template_part( 'components/button', null, $btnArgs ); ?>
+						</div>
+					<?php endif; ?>
+					<div class="header-menu-button">
+						<button type="button" title="Icon menu" class="icon-menu"><span></span></button>
+					</div>
+				</div>
+				</div>
+			</header>
 		<?php endif; ?>
